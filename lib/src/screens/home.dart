6c1af6f8d2738e66ed939c4de5a6856cab2321e1
mj <onyangomaureen95@gmail.com>
2019-01-30 12:35:49 +0300

    Add onTap method for gesture detector

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -4,42 +4,59 @@ import '../widgets/cat.dart';
 class Home extends StatefulWidget {
   HomeState createState() => HomeState();
 }
-class HomeState extends State<Home> with TickerProviderStateMixin{
- Animation<double>catAnimation;
- AnimationController catController;
 
- initState(){
-   super.initState();
-catController=AnimationController(
-  duration: Duration(milliseconds: 200),
-vsync: this,
-);
- catAnimation= Tween(begin: 0.0, end:100.0)
-.animate(
-  CurvedAnimation(
-    parent: catController,
-    curve: Curves.easeIn
-    ),
+class HomeState extends State<Home> with TickerProviderStateMixin {
+  Animation<double> catAnimation;
+  AnimationController catController;
+
+  initState() {
+    super.initState();
+    catController = AnimationController(
+      duration: Duration(milliseconds: 200),
+      vsync: this,
     );
-    
-
+    catAnimation = Tween(begin: 0.0, end: 100.0).animate(
+      CurvedAnimation(parent: catController, curve: Curves.easeIn),
+    );
+  }
+onTap(){
+  if(catController.status==AnimationStatus.completed){
+    catController.reverse();
+  } else if(catController.status==AnimationStatus.dismissed){
+    catController.forward();
+  }
 }
-
- Widget build(context) {
+  Widget build(context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Animation"),
+      appBar: AppBar(
+        title: Text("Animation"),
+      ),
+      body: GestureDetector(
+        child:Center(
+          child: Stack(
+            overflow: Overflow.visible,
+            children: [buildCatAnimation(),
+            ],
+          ),
         ),
-        body: Container(
-          child:buildCatAnimation(),
-        ), );
-}
-Widget buildCatAnimation(){
-  return AnimatedBuilder(animation: catAnimation,
-  builder: (context,child){
+      
+         onTap: onTap,
+      ),
+     
+    );
+  }
 
-  },
-  child: Cat(),
-  );
-}
+  Widget buildCatAnimation() {
+    return AnimatedBuilder(
+      animation: catAnimation,
+      builder: (context, child) {
+        return Positioned(
+          child: child,
+          top: catAnimation.value,
+          left: 0.0,
+        );
+      },
+      child: Cat(),
+    );
+  }
 }
