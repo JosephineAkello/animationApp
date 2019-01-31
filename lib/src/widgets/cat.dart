e40a97fc1857b4c31aa64345b268233a389d270c
mj <onyangomaureen95@gmail.com>
2019-01-31 15:52:36 +0300

    Format document

--- lib/src/widgets/cat.dart
+++ lib/src/widgets/cat.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 class Cat extends StatelessWidget{
 
   Widget build(context){
-    return Image.network('https://i.imgur.com/QwhZRyL.png',
+    return Image.network(
+      'https://i.imgur.com/QwhZRyL.png',
     );
   }
 }
