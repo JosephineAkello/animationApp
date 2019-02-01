537d52345730149ea1d9fd5c466ea4597109c469
mj <onyangomaureen95@gmail.com>
2019-02-01 15:28:21 +0300

    set debug mode banner to false

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -4,6 +4,7 @@ import 'screens/home.dart';
 class App extends StatelessWidget{
   Widget build(context){
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Animation',
       theme: ThemeData(
         primarySwatch: Colors.pink
